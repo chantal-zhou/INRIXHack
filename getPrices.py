4327eafd0d819681f955c3ef86bc5859b9863742
chantal-zhou <148928054+chantal-zhou@users.noreply.github.com>
2023-12-02 22:23:46 -0800

    Delete getPrices.py
    Moved to parkingSpot.py

--- getPrices.py
+++
@@ -1,8 +0,0 @@
-def getPrices():
-  result = dict()
-  for i in range(0,len(resp["result"])): 
-    test = resp["result"][i]
-    price = test["rateCard"]
-    location = test["name"]
-    result.update({location : price})
-  print(result)
