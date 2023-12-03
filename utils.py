6b5b0e401bedf2f1fdf2ddb5a06f8b16334ba8f0
chantal-zhou <148928054+chantal-zhou@users.noreply.github.com>
2023-12-03 02:30:14 -0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
-import json 
-
 def write_to_file(dictionary, name):
     with open(name + ".txt", 'w') as f:  
         for key, value in dictionary.items():  
             f.write('%s:%s\n' % (key, value))
+
+def sort_by_highest(dictionary):
+    return dict(reversed(sorted(dictionary.items(), key=lambda item: item[1])))
+
+def sort_by_lowest(dictionary):
+    return dict(sorted(dictionary.items(), key=lambda item: item[1]))
