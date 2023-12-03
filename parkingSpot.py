488960cc69a6af836a4da65af1965a3c364e5fb1
chantal-zhou <148928054+chantal-zhou@users.noreply.github.com>
2023-12-02 23:07:54 -0800

    Update parkingSpot.py

--- parkingSpot.py
+++ parkingSpot.py
@@ -7,6 +7,7 @@ class parkingSpot:
         for spot in resp["result"]:
             coordinates.update({spot["name"] : spot["point"]["coordinates"]})
         return coordinates
+    @staticmethod
     def getPrices(resp):
         prices = {}
         for spot in resp["result"]:
